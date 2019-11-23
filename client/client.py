e33336c7365771d8cf3697858cade3d2adae256a
TylerLyczak <tyler.m.lyczak@gmail.com>
2019-11-22 20:57:47 -0600

    Added GPIO support

--- client/client.py
+++ client/client.py
@@ -1,20 +1,30 @@
-# * ------ 
+# * ------
 # * File: /client.py
 # * File Created: Friday, 22nd November 2019 6:07:29 pm
-# * Author: Alex Chomiak 
-# * 
+# * Author: Alex Chomiak
+# *
 # * Last Modified: Friday, 22nd November 2019 7:05:14 pm
-# * Modified By: Alex Chomiak 
-# * 
+# * Modified By: Tyler Lyczak
+# *
 # * Author Github: https://github.com/alexchomiak
-# * ------ 
+# * ------
 
 import sys
 import socket
 import threading
 import time
+import RPi.GPIO as GPIO
+
 port = sys.argv[1]
 ip = sys.argv[2] if (len(sys.argv) > 2) else '127.0.0.1'
+
+GPIO.setmode(GPIO.BOARD)
+
+leftButton = 7
+rightButton = 11
+
+GPIO.setup(rightButton, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(leftButton, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 print("Connecting client...")
 print("Port:",port)
@@ -53,8 +63,12 @@ def send_increment(val): # * Use this function to send increment to game
 # ** Dummy Code **
 increment = 10
 while(connected):
-  if playerX == 0: increment = -increment
-  if playerX > 500: increment = -increment
-  print("Player at X:",playerX)
-  send_increment(increment)
+  buttonStateRight = GPIO.input(rightButton)
+  buttonStateLeft = GPIO.input(leftButton)
+
+  if buttonStateRight == False:
+      send_increment(increment)
+
+  if buttonStateLeft == False:
+      send_increment((-1)*increment)
   time.sleep(.05)
